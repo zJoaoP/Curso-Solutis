8e1447c272c9b610daa17a0e796df88a05726d7c
JoaoP <contato.joaocomp@gmail.com>
2020-12-15 15:34:36 -0300

    Rejecting promise on connection error

--- 14-12-2020/database.js
+++ 14-12-2020/database.js
@@ -7,7 +7,7 @@ const uri = `mongodb+srv://${process.env.USER}:${process.env.PASSWORD}@cluster0.
 
 export const db = new Promise((resolve, reject) => {
   MongoClient.connect(uri, (err, client) => {
-    if (err) resolve(console.log(err));
+    if (err) reject(console.log(err));
     resolve(client.db(process.env.DB_NAME));
   });
 });
