5df10247d3b22d18d26d3efa6a2089a151b48613
JoaoP <contato.joaocomp@gmail.com>
2020-12-22 14:29:10 -0300

    Fixing time string display

--- 21-12-2020/src/helpers/time.js
+++ 21-12-2020/src/helpers/time.js
@@ -1,6 +1,6 @@
 export function currentTimeToString() {
   const date = new Date();
-  return `${date.getDay()}/${date.getMonth() + 1}/${date.getFullYear()} - 
+  return `${date.getDate()}/${date.getMonth() + 1}/${date.getFullYear()} - 
           ${date.getHours()}:${date.getMinutes()}:${date.getSeconds()}`;
 }
 
